5ffc57128d482427c66e8cb07582c401d4a58286
nobody <nobody@517af487-6003-4fa2-a80c-81bfcb792c70>
2002-04-06 22:35:35 +0000

    This commit was manufactured by cvs2svn to create tag 'jsboard-0_5_1'.

---
+++ tags/jsboard-0_5_1/jsboard-0/include/plaza/desc.ph
@@ -0,0 +1,34 @@
+<?
+//include("include/header.ph");
+//include("include/menu.ph");
+?>
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+<title>PLAZA<? echo $sub_title ?></title>
+<style type="text/css">
+<!--
+a:link, a:visited, a:active { text-decoration: none; }
+a:hover { text-decoration:underline; }
+td { font-size: 10pt; }
+-->
+</style>
+</head>
+
+<body bgcolor="#eeeeee" text="black" link="blue" vlink="">
+
+<br><br>
+<center>
+<font face=돋움 size=+2><b>PLAZA
+</b>
+</font>
+</center>
+
+<? if(!$ndesc) : ?>
+
+<!-- 글쓰기, 답장쓰기등의 화면에 나올 글을 씁니다. -->
+
+<? endif ?>
+
+<br>
---
+++ tags/jsboard-0_5_1/jsboard-0/plaza.sql
@@ -0,0 +1,32 @@
+# MySQL dump 6.0
+#
+# Host: localhost    Database: board
+#--------------------------------------------------------
+# Server version	3.22.23b
+
+#
+# Table structure for table 'plaza'
+#
+CREATE TABLE java (
+  no int(8) DEFAULT '0' NOT NULL auto_increment,
+  num int(8) DEFAULT '0' NOT NULL,
+  date int(11) DEFAULT '0' NOT NULL,
+  host tinytext,
+  name tinytext,
+  passwd varchar(13),
+  email tinytext,
+  url tinytext,
+  title tinytext,
+  text mediumtext,
+  refer int(8) DEFAULT '0' NOT NULL,
+  reyn int(1) DEFAULT '0' NOT NULL,
+  reno int(8) DEFAULT '0' NOT NULL,
+  rede int(8) DEFAULT '0' NOT NULL,
+  reto int(8) DEFAULT '0' NOT NULL,
+  bofile varchar(100),
+  bcfile varchar(100),
+  bfsize int(4),
+  PRIMARY KEY (no),
+  KEY num (num),
+  KEY reno (reno)
+);
