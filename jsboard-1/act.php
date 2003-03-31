0a4eb1d5355bc4d15f939ff631da6de853ce9347
JoungKyun Kim <joungkyun@daum.net>
2003-03-31 04:36:40 +0000

    fixed bug of edit mode

--- jsboard-1/act.php
+++ jsboard-1/act.php
@@ -232,22 +232,15 @@ if ($o[at] != "dn" && $o[at] != "sm" && $o[at] != "se" && $o[at] != "ma") {
           unlink("data/$table/files/$fdelinfo[bcfile]/$fdelinfo[bofile]");
           rmdir("data/$table/files/$fdelinfo[bcfile]");
         }
+        $upquery = ",\n        bofile = '$upfile[name]', bcfile = '$bfilename', bfsize = '$upfile[size]'\n";
       }
-
-      sql_query("
-        UPDATE $table SET date = '$atc[date]', host = '$atc[host]',
-        name = '$atc[name]', email = '$atc[email]', url = '$atc[url]',
-        title = '$atc[title]', text = '$atc[text]', html = '$atc[html]',
-        bofile = '$upfile[name]', bcfile = '$bfilename',
-        bfsize = '$upfile[size]'
-        WHERE no = '$atc[no]'");
-    } else {
-      sql_query("
-        UPDATE $table SET date = '$atc[date]', host = '$atc[host]',
-        name = '$atc[name]', email = '$atc[email]', url = '$atc[url]',
-        title = '$atc[title]', text = '$atc[text]', html = '$atc[html]'
-        WHERE no = '$atc[no]'");
     }
+
+    sql_query("
+      UPDATE $table SET date = '$atc[date]', host = '$atc[host]',
+      name = '$atc[name]', email = '$atc[email]', url = '$atc[url]',
+      title = '$atc[title]', text = '$atc[text]', html = '$atc[html]'$upquery
+      WHERE no = '$atc[no]'");
 
     set_cookie($atc);
 
