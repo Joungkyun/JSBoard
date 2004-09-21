0034eadf53db80dac2f9071ef3ba9afeb00734c8
oops <oops@517af487-6003-4fa2-a80c-81bfcb792c70>
2004-09-21 16:59:17 +0000

    Support RSS version 2.0

--- trunk/jsboard-2/rss.php
+++ trunk/jsboard-2/rss.php
@@ -45,9 +45,35 @@ $result = sql_db_query ($db['name'], $sql);
 $i = 0;
 
 while( $rss_article[$i] = sql_fetch_array($result) ) {
-  $rss_article[$i]['link'] = "{$board['path']}read.php?table={$table}&no={$rss_article[$i]['no']}";
-  $rss_article[$i]['text'] = "<span style=\"font-size:11px;\"><pre>{$rss_article[$i]['text']}</pre></span>";
-  $rss_article[$i]['date'] = date("Y-m-d",$rss_article[$i]['date'] ) . 'T' . date("H:i:sO", $rss_article[$i]['date']);
+  $rss_article[$i]['link'] = "{$board['path']}read.php?table={$table}&amp;no={$rss_article[$i]['no']}";
+  $rss_article[$i]['date'] = date ("r", $rss_article[$i]['date']);
+  #$rss_article[$i]['date'] = date("Y-m-d",$rss_article[$i]['date'] ) . 'T' . date("H:i:sO", $rss_article[$i]['date']);
+
+  $rss_article[$i]['text'] = preg_replace ("!\n!", "<br />\n", $rss_article[$i]['text']);
+  $rss_article[$i]['text'] = auto_link ($rss_article[$i]['text']);
+
+  $_body = "<table width=\"100%\" border=0 cellpadding=0 cellspacing=1>\n" .
+           "<tr><td bgcolor=#000000>\n" .
+           "<table width=\"100%\" border=0 cellpadding=3 cellspacing=1>\n" .
+           "<tr><td style=\"font: 12px 굴림체; font-weight: bold; color: #ffffff\">\n" .
+           "REPLY : <a href={$board['path']}reply.php?table={$table}&no={$rss_article[$i]['no']} style=\"color: #ffffff; text-decoration: none;\">" .
+           "{$board['path']}reply.php?table={$table}&no={$rss_article[$i]['no']}</a><br />\n" .
+           "DELETE: <a href={$board['path']}delete.php?table={$table}&no={$rss_article[$i]['no']} style=\"color: #ffffff; text-decoration: none;\">" .
+           "{$board['path']}delete.php?table={$table}&no={$rss_article[$i]['no']}</a>\n" .
+           "</td></tr>\n" .
+           "</table>\n" .
+           "</td></tr>\n" .
+           "<tr><td bgcolor=#000000>\n" .
+           "<table width=\"100%\" border=0 cellpadding=3 cellspacing=1>\n" .
+           "<tr><td bgcolor=#ffffff style=\"font-size: 11px;\"><pre>\n" .
+           "{$rss_article[$i]['text']}\n" .
+           "</pre></td></tr>\n" .
+           "</table>\n" .
+           "</td></tr>\n" .
+           "</table>\n";
+
+  $_body = htmlspecialchars ($_body, ENT_QUOTES);
+  $rss_article[$i]['text'] = $_body;
 
   if (!$rss_article[$i]['name'])
     $rss_article[$i]['name'] = $rss_article[$i]['rname'];
@@ -59,6 +85,11 @@ $rss_article_num = $i;
 unset($result, $i);
 mysql_close();
 
+$now = time ();
+$cYear = date ("Y", $now);
+$bdate = date ("r", $now);
+$_charset = strtolower ($langs['charset']);
+
 # 여기 까지 이상이 없으면, 실제 RSS 출력
 #
 header ('Cache-Control: private, pre-check=0, post-check=0, max-age=0');
@@ -66,47 +97,33 @@ header ('Expires: ' . gmdate('D, d M Y H:i:s', time()) . ' GMT');
 header ('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 header ('Content-Type: text/xml');
 
-echo "<?xml version=\"1.0\" encoding=\"{$langs[charset]}\"?>\n";
+echo "<?xml version=\"1.0\" encoding=\"{$_charset}\"?>\n";
 ?>
-<rdf:RDF xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://purl.org/rss/1.0/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:wiki="http://purl.org/rss/1.0/modules/wiki/">
-  <channel rdf:about="<?=$rss['link']?>">
+<rss version="2.0">
+  <channel>
     <title><?=$rss['channel']?></title>
     <link><?=$rss['link']?></link>
     <description><?=$board['title']?></description>
-    <items>
-     <rdf:Seq>
+    <language><?=$_charset?></language>
+    <copyright>1999-<?=$cYear?> JSBoard Open Project</copyright>
+    <lastBuildDate><?=$bdate?></lastBuildDate>
+    <generator>JSBoard <?=$board['ver']?></generator>
+
 <?php
-for ($i = 0;  $i < $rss_article_num ; $i ++){
-	$rss_article[$i]['link'] = str_replace("&", "&amp;", $rss_article[$i]['link']);
-?>
-      <rdf:li rdf:resource="<?=$rss_article[$i]['link']?>" /> 
-<?php
+for ( $i=0; $i<$rss_article_num; $i++ ) {
+  echo "<item>\n" .
+       "  <title>{$rss_article[$i]['title']}</title>\n" .
+       "  <link>{$rss_article[$i]['link']}</link>\n";
+
+  if ( $rss['is_des'] ) {
+    echo "  <description>{$rss_article[$i]['text']}</description>\n";
+  }
+
+  echo "  <pubDate>{$rss_article[$i]['date']}</pubDate>\n" .
+       "  <author>{$rss_article[$i]['name']}</author>\n" .
+       "</item>\n";
 }
 ?>
-     </rdf:Seq>
-    </items>
-   </channel>
-<?php
-for ($i = 0 ; $i < $rss_article_num; $i ++){
-?>
-<item rdf:about="<?=$rss_article[$i]['link']?>">
- <title><?=$rss_article[$i]['title']?></title> 
- <link><?=$rss_article[$i]['link']?></link> 
-<?php
-	if ($rss['is_des']) {
-?>
- <description><?=htmlspecialchars($rss_article[$i]['text'])?></description>
-<?php
-	}
-?>
- <dc:date><?=$rss_article[$i]['date']?></dc:date> 
- <dc:contributor>
- <rdf:Description>
- <rdf:value><?=$rss_article[$i]['name']?></rdf:value> 
- </rdf:Description>
- </dc:contributor>
-</item>
-<?php
-}
-?>
-</rdf:RDF>
+
+  </channel>
+</rss>
