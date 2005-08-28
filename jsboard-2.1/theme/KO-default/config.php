cdf619feb0a91efdc1243a8462ae4f6d2ccf7b00
JoungKyun Kim <joungkyun@daum.net>
2005-08-28 18:20:33 +0000

    update theme

--- jsboard-2.1/theme/KO-default/config.php
+++ jsboard-2.1/theme/KO-default/config.php
@@ -2,12 +2,12 @@
 ##############################################################
 # Theme Version
 ##############################################################
-$designer['ver'] = "A9";
+$designer['ver'] = 'A9';
 
 ##############################################################
 # Language configuration
 ##############################################################
-$_code          = "ko";				# 게시판 언어
+$_code          = 'ko';				# 게시판 언어
 
 ##############################################################
 # Addon Design configuration
@@ -15,7 +15,7 @@ $_code          = "ko";				# 게시판 언어
 # 글 리스트 tr 의 높이 설정
 # list table 에서는 image 의 height 로 조정. 예를 들어
 # <img src="img/blank.gif" width=0 height=$line_height border=0 alt=''>
-$lines['height'] = "25";
+$lines['height'] = '25';
 
 # 글 리스트들 사이에 디자인을 넣기위한 코드
 # $lines['design'] 설정은 <TD></TD> 설정으로 이루어 짐. <TD> 에
@@ -24,28 +24,28 @@ $lines['height'] = "25";
 # 이용하여 테스트 해 보기 바람
 # $lines['comment'] 는 comment rows 에 사용되며 <tr></tr> 로 설
 # 정 함. COLSPAN=AA 는 필요 없음.
-#$lines['design'] = "<TD COLSPAN=AA><hr></TD>";
-$lines['design'] = "";
-$lines['comment_design'] = "<tr>\n" .
-  "<td colspan=4 style=\"background-image: url(./images/dotline.gif)\">" .
-  "<img src=\"./images/blank.gif\" width=1 height=4 border=0 alt=''>" .
-  "</td></tr>";
+#$lines['design'] = '<TD COLSPAN=AA><hr></TD>';
+$lines['design'] = '';
+$lines['comment_design'] = '<tr>\n' .
+  '<td colspan=4 style="background-image: url(./images/dotline.gif)">' .
+  '<img src="./images/blank.gif" width=1 height=4 border=0 alt="">' .
+  '</td></tr>';
 
 ##############################################################
 # Width Configuration
 ##############################################################
 
 # list paget table ratio ===================================
-$td_width['no'] = "5%";				# 번호 필드 너비
+$td_width['no'] = '5%';				# 번호 필드 너비
 
 # 제목 필드 너비
-if ($upload['yesno'] && $cupload['yesno']) $td_width['title'] = "54%";
-else $td_width['title'] = "63%";
+if ($upload['yesno'] && $cupload['yesno']) $td_width['title'] = '54%';
+else $td_width['title'] = '63%';
 
-$td_width['name']   = "14%";		# 글쓴이 필드 너비
-$td_width['upload'] = "9%";			# 파일 필드 너비
-$td_width['dates']  = "13%";		# 날짜 필드 너비
-$td_width['refer']  = "5%";			# 읽은수 필드 너비
+$td_width['name']   = '14%';		# 글쓴이 필드 너비
+$td_width['upload'] = '9%';			# 파일 필드 너비
+$td_width['dates']  = '13%';		# 날짜 필드 너비
+$td_width['refer']  = '5%';			# 읽은수 필드 너비
 
 ##############################################################
 # Field Array Configuration
@@ -58,5 +58,5 @@ $td_width['refer']  = "5%";			# 읽은수 필드 너비
 # D  -> 글등록 날짜
 # R  -> 읽은 수
 #
-$td_array = "nTNFDR";
+$td_array = 'nTNFDR';
 ?>
