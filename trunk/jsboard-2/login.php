1a38655a2694b95e21e49231de966ce92a9ad942
oops <oops@517af487-6003-4fa2-a80c-81bfcb792c70>
2006-04-30 10:55:21 +0000

    fixed XSS attack security problems on login.php with $table variables

--- trunk/jsboard-2/login.php
+++ trunk/jsboard-2/login.php
@@ -9,8 +9,20 @@ parse_query_str();
 $agent = get_agent();
 if(preg_match("/links|w3m|lynx/i",$agent['br'])) $textBrowser = 1;
 
-if($table && file_exists("data/$table/config.php"))
-  { include "data/$table/config.php"; }
+$table = trim ($table);
+
+if ($table) {
+  if (!preg_match("/^[a-z]/i", $table))
+    print_error("$name Value must start with an alphabet",250,150,1);
+  if (preg_match("/[^a-z0-9_-]/i",$table))
+    print_error("Can't use special characters except alphabat, numberlic , _, - charcters",250,150,1);
+  if (preg_match("/^as$/i",$name))
+    print_error("Cat't use table name as &quot;as&quot;",250,150,1);
+}
+
+if($table && file_exists("data/$table/config.php")) {
+  include "data/$table/config.php";
+}
 
 # input 문의 size를 browser별로 맞추기 위한 설정
 $size = form_size(6);
