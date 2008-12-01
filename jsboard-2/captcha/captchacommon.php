464dde2584b497a171f8df0a3625639fa82eebcc
JoungKyun Kim <joungkyun@daum.net>
2008-12-01 20:01:53 +0000

    support captcha

---
+++ jsboard-2/captcha/captchacommon.php
@@ -0,0 +1,41 @@
+<?
+if ( $board['captcha'] ) {
+	require_once 'captcha/captcha.php';
+	$capt = new Captcha ($board['captcha']);
+
+	if ( $capt->disable === true ) {
+		$pcaptcharcolspan = ' colspan=3';
+		return;
+	}
+
+    if ( $board['super'] || $board['adm'] ) {
+		$pcaptcharcolspan = ' colspan=3';
+		$capt->disable = true;
+		return;
+    }
+
+	if ( preg_match ('/^[24-7]$/', $board['mode']) ) {
+		$pcaptcharcolspan = ' colspan=3';
+		$capt->disable = true;
+		return;
+	}
+
+	$ckey = $capt->captchakey ();
+	$captsize = $size['pass'] * 2;
+
+	$pcaptcha = <<<EOF
+</td>
+<td style="width: 88px;">
+<script type="text/javascript">
+function captcha_insert() {
+	document.getElementById('id_ckeyv').value = "{$capt->captchadata ($ckey)}";
+}
+</script>
+<img src="./captcha/captchaimg.php?{$ckey}" alt="Input {$capt->captchadata($ckey)}" onclick="captcha_insert(); return false;"></td>
+<td valign="top">
+{$_('captstr')}<br />
+<input type="text" size="{$captsize}" name="atc[ckeyv]" id="id_ckeyv" onclick="captcha_insert(); return false;">
+<input type="hidden" name="atc[ckey]" value="{$ckey}">
+EOF;
+}
+?>
---
+++ jsboard-2/captcha/captchaimg.php
@@ -0,0 +1,20 @@
+<?
+require_once ('../config/global.php');
+require_once ('./captcha.php');
+
+if ( ! $board['captcha'] )
+	exit;
+
+$capt = new Captcha ($board['captcha']);
+#$key = $_GET['k'] ? $_GET['k'] : $capt->captchakey ();
+
+if ( $_SERVER['argv'][0] )
+	$key = $_SERVER['argv'][0];
+else if ( is_int ($_SERVER['QUERY_STRING']) && $_SERVER['QUERY_STRING'] < 1000 ) {
+	$key = $_SERVER['QUERY_STRING'];
+} else
+	$key = $capt->captchakey ();
+
+$capt->print_img_header ();
+$capt->create_captcha ($capt->captchadata ($key));
+?>
