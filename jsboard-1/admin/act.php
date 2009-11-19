c4a91a486d54a9d6775788d70af204cb4dba86da
JoungKyun Kim <joungkyun@daum.net>
2009-11-19 17:08:56 +0000

    fixed that failed to create table

--- jsboard-1/admin/act.php
+++ jsboard-1/admin/act.php
@@ -1,26 +1,26 @@
 <?php
-# $Id: act.php,v 1.8 2009-11-19 13:55:41 oops Exp $
-include "./include/admin_head.ph";
-include "../include/ostype.ph";
+# $Id: act.php,v 1.9 2009-11-19 17:08:55 oops Exp $
+include './include/admin_head.ph';
+include '../include/ostype.ph';
 
 # password 비교함수 - admin/include/auth.ph
 compare_pass($sadmin,$login);
 
-if(preg_match("/links|w3m|lynx/i",$agent[br])) $textBrowser = 1;
+if(preg_match('/links|w3m|lynx/i',$agent['br'])) $textBrowser = 1;
 
-$connect=mysql_connect($db[server],$db[user] ,$db[pass])  or  
-              die("$langs[sql_na]" ); 
+$connect=mysql_connect($db['server'],$db['user'] ,$db['pass'])  or  
+              die($langs['sql_na']); 
 
 # db_name이 존재하지 않으면 아래를 출력합니다.
-exsit_dbname_check($db[name]);
+exsit_dbname_check($db['name']);
 
-mysql_select_db($db[name],$connect);
+mysql_select_db($db['name'],$connect);
 
-if( $mode != "manager_config") {
+if( $mode != 'manager_config') {
 
   # 알파벳 구분된 페이지에서 넘어 왔을 경우 페이지를
   # 되돌리기 위해 지정
-  if($ts) $tslink = "?ts=$ts";
+  if($ts) $tslink = '?ts=' . $ts;
 
   ###########################################
   #          DB에 접속
@@ -30,32 +30,32 @@ if( $mode != "manager_config") {
     table_name_check($table_name);
 
     # table delete
-    $table_del = "drop table $table_name";
+    $table_del = 'drop table ' . $table_name;
     $result = mysql_query($table_del,$connect);
 
     # 게시판 계정에서 사용되는 file 삭제
-	unlink_r("../data/{$table_name}");
+    unlink_r("../data/{$table_name}");
     mysql_close();
   }
 
   if($mode == 'db_create')  {
-    $tbl_list = mysql_list_tables($db[name]);
+    $tbl_list = mysql_list_tables($db['name']);
 
     # 새로만들 계정이름의 존재유무 체크
     table_name_check($new_table);
     # table list 존재 유무 체크
-    table_list_check($db[name]);
+    table_list_check($db['name']);
     # 동일한 이름의 게시판이 있는지 확인
     same_db_check($tbl_list,$new_table);
 
-    $create_table = "CREATE TABLE $new_table ( 
-              no int(6) DEFAULT '0' NOT NULL auto_increment,
+    $create_table = "CREATE TABLE {$new_table} ( 
+              no int(6) NOT NULL auto_increment,
               num int(6) DEFAULT '0' NOT NULL,
               idx int(6) DEFAULT '0' NOT NULL,
               date int(11) DEFAULT '0' NOT NULL,
               host tinytext,
               name tinytext,
-              passwd varchar($ostypes[pfield]),
+              passwd varchar({$ostypes['pfield']}),
               email tinytext,
               url tinytext,
               title tinytext,
@@ -80,61 +80,68 @@ if( $mode != "manager_config") {
 
     $passwd_ext = crypt($passwd_ext);
 
-    $insert_data = "insert into $new_table values ('',1,1,$date,'$host_ext','$name_ext','$passwd_ext',
-                    '$email_ext','$url_ext','$subj_msg','$text_msg',0,0,0,0,0,0,0,'','','')";
+    $insert_data = "INSERT INTO {$new_table}
+                         VALUES (
+                           '',1,1,{$date},'{$host_ext}','{$name_ext}','{$passwd_ext}',
+						   '{$email_ext}','{$url_ext}','{$subj_msg}','{$text_msg}',
+                           0,0,0,0,0,0,0,'','',''
+                         )";
 
     $result = mysql_query($create_table, $connect);
     $result_insert = mysql_query($insert_data, $connect);
 
     # 새로운 게시판에 필요한 파일및 디렉토리 생성
-    mkdir("../data/$new_table",0700);
-    mkdir("../data/$new_table/$upload[dir]",0700);
-    chmod("../data/$new_table",0755);
-    chmod("../data/$new_table/$upload[dir]",0755);
-    copy("../Installer/sample/$ostypes[name]/config.ph","../data/$new_table/config.ph");
-    chmod("../data/$new_table/config.ph",0644);
-    copy("../Installer/sample/$ostypes[name]/html_head.ph","../data/$new_table/html_head.ph");
-    chmod("../data/$new_table/html_head.ph",0644);
-    copy("../Installer/sample/$ostypes[name]/html_tail.ph","../data/$new_table/html_tail.ph");
-    chmod("../data/$new_table/html_tail.ph",0644);
+    mkdir("../data/{$new_table}",0700);
+    mkdir("../data/{$new_table}/{$upload['dir']}",0700);
+    chmod("../data/{$new_table}",0755);
+    chmod("../data/{$new_table}/{$upload['dir']}",0755);
+    copy ("../Installer/sample/{$ostypes['name']}/config.ph",
+          "../data/{$new_table}/config.ph");
+    chmod("../data/{$new_table}/config.ph",0644);
+    copy ("../Installer/sample/{$ostypes['name']}/html_head.ph",
+          "../data/{$new_table}/html_head.ph");
+    chmod("../data/{$new_table}/html_head.ph",0644);
+    copy ("../Installer/sample/{$ostypes['name']}/html_tail.ph",
+          "../data/{$new_table}/html_tail.ph");
+    chmod("../data/{$new_table}/html_tail.ph",0644);
 
     # 현재 디렉토리를 변경
-    chdir("../data/$new_table");
-    if(file_exists("default.themes")) unlink("default.themes");
-    symlink("../../config/themes/basic.themes","default.themes");
-    chdir("../../admin");
+    chdir("../data/{$new_table}");
+    if(file_exists('default.themes')) unlink('default.themes');
+    symlink('../../config/themes/basic.themes','default.themes');
+    chdir('../../admin');
 
     mysql_close();
   }
 
-  if($mode == "global_chg") {
+  if($mode == 'global_chg') {
 
     # quot 변환된 문자를 un quot 한다
-    $vars = "<?\n".stripslashes($glob[vars])."\n?>";
-    $spam = stripslashes($glob[spam]);
-    $br   = stripslashes($glob[brlist]);
+    $vars = "<?\n".stripslashes($glob['vars'])."\n?>";
+    $spam = stripslashes($glob['spam']);
+    $br   = stripslashes($glob['brlist']);
 
-    file_operate("../config/global.ph","w",0,$vars);
-    file_operate("../config/spam_list.txt","w",0,$spam);
-    file_operate("../config/allow_browser.txt","w",0,$br);
+    file_operate('../config/global.ph','w',0,$vars);
+    file_operate('../config/spam_list.txt','w',0,$spam);
+    file_operate('../config/allow_browser.txt','w',0,$br);
 
     # 현재 디렉토리를 변경
-    chdir("../config");
-    if(file_exists("default.themes")) unlink("default.themes");
-    symlink("themes/$glob[theme].themes","default.themes");
+    chdir('../config');
+    if(file_exists('default.themes')) unlink('default.themes');
+    symlink("themes/{$glob['theme']}.themes",'default.themes');
 
-    $langs[act_complete] = str_replace("\n","\\n",$langs[act_complete]);
-    $langs[act_complete] = str_replace("'","\'",$langs[act_complete]);
+    $langs['act_complete'] = str_replace("\n","\\n",$langs['act_complete']);
+    $langs['act_complete'] = str_replace("'","\'",$langs['act_complete']);
 
     if(!$textBrowser) {
-      echo "<script>\n" .
-           "alert('$langs[act_complete]')\n" .
+      echo "<script type=\"text/javascript\">\n" .
+           "alert('{$langs['act_complete']}')\n" .
            "window.close()\n</script>";
       exit;
     }
   }
 
-  Header("Location:admin.php$tslink");
+  Header("Location:admin.php{$tslink}");
 
 } else {
   if($admincenter_pass && $readmincenter_pass) {
@@ -142,17 +149,20 @@ if( $mode != "manager_config") {
 
       # 입력 받은 패스워드를 crypt 암호화
       $ad_pass = crypt($admincenter_pass);
-      $ad_pass = str_replace("\$","\\\$",$ad_pass);
 
-      $configfile = "./include/config.ph";
-      $admininfo = file_operate($configfile,"r","Don't open $configfile");
-      $admininfo = eregi_replace("sadmin\[passwd\] = (\"[a-z0-9\.\/\$]*\")","sadmin[passwd] = \"$ad_pass\"",$admininfo);
+      $configfile = './include/config.ph';
+      $admininfo = file_operate($configfile,'r',"Don't open {$configfile}");
+      $admininfo = preg_replace(
+                                "/(passwd['\"]?\][ \t]*=[ \t]*['\"])[^'\"]*(['\"];)/",
+                                "\\1{$ad_pass}\\2",
+                                $admininfo
+      );
 
-      file_operate($configfile,"w","Can't update $configfile",$admininfo);
+      file_operate($configfile,'w',"Can't update {$configfile}",$admininfo);
 
       complete_adminpass();
     } else admin_pass_error();
-  } else print_error($langs[a_act_cp]);
+  } else print_error($langs['a_act_cp']);
 }
 
 ?>
--- jsboard-1/admin/include/first_reg.ph
+++ jsboard-1/admin/include/first_reg.ph
@@ -1,11 +1,12 @@
-<?
+<?php
+# $Id: first_reg.ph,v 1.4 2009-11-19 17:08:56 oops Exp $
 # 새로운 게시판 생성시에 1번글로 등록될 내용들
 $date = time();
-$host_ext = "127.0.0.1";
-$name_ext = "$langs[first1]";
-$passwd_ext = "lHJTjGW8VhHc.";
-$email_ext = "";
-$url_ext = "http://www.oops.org";
-$subj_msg = "$langs[first2]";
-$text_msg = "$langs[first3]";
+$host_ext = '127.0.0.1';
+$name_ext = $langs['first1'];
+$passwd_ext = 'lHJTjGW8VhHc.';
+$email_ext = '';
+$url_ext = 'http://oops.org/';
+$subj_msg = $langs['first2'];
+$text_msg = $langs['first3'];
 ?>
