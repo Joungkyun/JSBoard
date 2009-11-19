eff6e253fd4999b659c9299c4655813b6056101d
oops <oops@517af487-6003-4fa2-a80c-81bfcb792c70>
2009-11-19 16:06:48 +0000

    fixed session.bug_compat_42 problems

--- trunk/jsboard-1/admin/session.php
+++ trunk/jsboard-1/admin/session.php
@@ -7,6 +7,19 @@ if(!$parse_query_str_check) parse_query_str();
 include "include/config.ph";
 include "include/check.ph";
 
+#
+# php 4.2.3 상위 버전에서 session 관련 warning이 발생한다.
+# 이를 발생하지 않게 하기 위해서 4.2.3보다 버전이 높을 경우
+# session.bug_compat_42를 off 한다.
+#
+# version_compare 함수가 4.1.0 부터 지원하기 때문에 version_compare
+# 함수가 지원하지 않는다면, 무시해도 상관 없는 코드가 된다.
+#
+if (function_exists('version_compare')) {
+  if (version_compare(phpversion(), '4.2.3', '>'))
+    ini_set ('session.bug_compat_42', false);
+}
+
 if ($mode == "login") {
   $login[pass] = compare_pass($sadmin,$logins,1);
   # 세션 등록
