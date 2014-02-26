0f4e0517c43328a62840e8b131585d1c4a805893
oops <oops@517af487-6003-4fa2-a80c-81bfcb792c70>
2014-02-26 17:50:18 +0000

    [#318733] fixed session_register and session_destroy problems on PHP 5.5

--- trunk/jsboard-2/INSTALLER/session.php
+++ trunk/jsboard-2/INSTALLER/session.php
@@ -1,18 +1,18 @@
 <?php
-# $Id: session.php,v 1.10 2014-02-26 17:09:10 oops Exp $
+# $Id: session.php,v 1.11 2014-02-26 17:50:18 oops Exp $
 session_start(); // session을 시작한다.
 require_once '../include/print.php';
 parse_query_str();
 if ($mode == "login") {
-  session_register("mysqlpass"); //세션 등록한다.
+  $_SESSION['mysqlpass'] = $mysqlpass; //세션 등록한다.
   header("Location: install.php?langss=$langss");
   exit;
 } elseif ($mode == "logout") {
-  session_destroy(); // 세션을 삭제한다
+  unset ($_SESSION['mysqlpass']); // 세션을 삭제
   header("Location: auth.php?mode=first&langss=$langss");
   exit;
 } elseif ($mode == "first") {
-  session_destroy(); // 세션을 삭제한다
+  unset ($_SESSION['mysqlpass']); // 세션을 삭제
   $langs['code'] = ($langss == "ko") ? "ko" : "en";
 
   $path['type'] = "Install";
