4d4403ce02968cf9adea5971e1b6407fe3b0f6f9
JoungKyun Kim <joungkyun@daum.net>
2016-01-15 07:42:30 +0000

    version update to 2.0.17 and fixed syntax error

--- jsboard-2/include/version.php
+++ jsboard-2/include/version.php
@@ -1,5 +1,5 @@
 <?php
-# $Id: version.php,v 1.19 2014-03-04 21:36:56 oops Exp $
-$board['ver'] = "2.0.16-dev";
-$theme['ver'] = "A10"
+# $Id: version.php,v 1.20 2016-01-15 07:42:30 oops Exp $
+$board['ver'] = '2.0.17';
+$theme['ver'] = 'A10';
 ?>
