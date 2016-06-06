8bd9c2964d04324ee6b1cb71c45066ef1da4edb6
JoungKyun Kim <joungkyun@daum.net>
2016-06-06 17:51:22 +0000

    cleanup after svn migration

---
+++ Installer/auth.php
@@ -0,0 +1,236 @@
+<?php
+
+$path[type] = "Install";
+$copydate = time();
+$copydate = date("Y",$copydate);
+
+require("../config/themes/basic.themes");
+require("./include/passwd.ph");
+
+if ($langss == "ko") $langs[code] = "ko";
+else $langs[code] = "en";
+
+include "../include/lang.ph";
+include "../include/get.ph";
+include "../include/print.ph";
+include "../html/head.ph";
+
+$agent = get_agent();
+
+echo "<table border=0 width=100% height=100%>\n" .
+        "<tr><td align=center valign=center>\n\n";
+
+if (!$mode) {
+  echo "<form method=POST action=$PHP_SELF>\n\n" .
+       "<table width=400 border=0 cellpadding=5>\n" .
+       "<tr><td bgcolor=$color[l0_bg] align=center>\n" .
+       "<font color=$color[l0_fg]>JSBoard Installer</font>\n" .
+       "</td></tr>\n<tr><td align=center>\n" .
+       "<font color=$color[text]>\n&nbsp;<br>\n" .
+       "<input type=radio name=langss checked value=\"ko\" id=radio>Korean\n" .
+       "<input type=radio name=langss value=\"en\" id=radio>English<br>\n" .
+       "<br>&nbsp;\n</font>\n</td></tr>\n" .
+       "<tr><td bgcolor=$color[l0_bg] align=center>\n" .
+       "<input type=hidden name=mode value=license>\n" .
+       "<input type=submit value=submit>\n" .
+       "</td></tr>\n</table>\n</form>\n";
+} elseif ($mode == "license") {
+  if ($langs[code] == "ko") $agreefile = "../COPYING";
+  else $agreefile = "../COPYING.en";
+
+  $agree_ment = eregi_replace("(http://jsboard.kldp.org)","<a href=\\1 target=_blank>\\1</a>",$agree_ment);
+
+  echo "<form name=auth method=POST action=auth.php>\n" .
+       "<table width=500 border=0 cellpadding=5>\n" .
+       "<tr><td bgcolor=$color[l0_bg] align=center>\n" .
+       "<font color=$color[l0_fg]>JSBoard License</font>\n" .
+       "</td></tr>\n<tr><td>\n";
+
+  if (eregi("MSIE",$agent[br])) {
+    echo "<iframe src=\"$agreefile\" width=600 height=200 frameborder=1>\n" .
+         "</iframe>\n";
+  } else {
+    $agree_ment = file_operate($agreefile,"r");
+
+    if ($langs[code] == "en") $colsize = form_size(40);
+    else $colsize = form_size(35);
+
+    echo "<textarea cols=$colsize rows=15 wrap=hard>$agree_ment</textarea>\n";
+  }
+
+  echo "</td></tr>\n" .
+       "<tr><td bgcolor=$color[l0_bg] align=center>\n" .
+       "<input type=hidden name=mode value=check_exec>\n" .
+       "<input type=hidden name=langss value=$langs[code]>\n" .
+       "<input type=submit value='AGREE'>\n" .
+       "</td></tr>\n</table>\n" .
+       "</form>\n";
+} elseif ($mode == "check_exec") {
+  echo "<table width=500 border=0 cellpadding=5>\n" .
+       "<tr><td bgcolor=$color[l0_bg] align=center>\n" .
+       "<font color=$color[l0_fg]>JSBoard Environment Check</font>\n" .
+       "</td></tr>\n<tr><td align=center>\n<font color=white>\n";
+
+  $mcheck = @mysql_connect($mysql_sock, "root", "$passwd");
+
+  if ($mcheck) {
+    mysql_select_db("mysql");
+    $query = "select * from db";
+    $result = mysql_query($query,$mcheck);
+    $column = mysql_fetch_array($result);
+    $cnum = sizeof($column)/2;
+    $mcheck = 1;
+  } else {
+    $mcheck = 0;
+    $cnum = 0;
+  }
+
+  if (exec("echo hello")) {
+    $echeck = 1;
+    exec("cat $apache_config_file | grep DirectoryIndex",$array);
+
+    for($i=0;$i<sizeof($array);$i++) {
+      $array[$i] = trim($array[$i]);
+      if(eregi("^Directory",$array[$i]) && eregi("index.(php |php$)",$array[$i])) $cindex = 1;
+    }
+  } else {
+    $echeck = 0;
+    $cindex = 0;
+    $cconf  = 0;
+  }
+
+  if(@touch("../data/aaa.test")) {
+    $p1 = 1;
+    @unlink("../data/aaa.test");
+  }
+
+  if(@touch("../config/aaa.test")) {
+    $p2 = 1;
+    @unlink("../config/aaa.test");
+  }
+
+  if ($p1 && $p2) $pcheck = 1;
+  else $pcheck = 0;
+
+  echo "</font>\n$langs[waitm]\n" .
+       "<meta http-equiv=\"refresh\" content=\"5;URL=$PHP_SELF?mode=check_conform&mcheck=$mcheck&cnum=$cnum&echeck=$echeck&cindex=$cindex&cconf=$cconf&pcheck=$pcheck&langss=$langs[code]\">" .
+       "</td></tr>\n" .
+       "<tr><td bgcolor=$color[l0_bg] align=center>\n" .
+       "<font color=$color[l0_fg]>$langs[wait]</font>\n" .
+       "</td></tr>\n</table>\n";
+} elseif ($mode == "check_conform") {
+
+  if ($mcheck) {
+    $m = "OK";
+    if ($cnum == "13") $mc = "OK";
+    else $mc = "Error";
+  } else $m = "Failed";
+
+  if ($echeck) {
+    $e = "OK";
+    if ($cindex) $ci = "OK";
+    else $ci = "Failed";
+  } else $e = "Failed";
+
+  if ($pcheck) $p = "OK";
+  else $p = "Error";
+
+  if (!$mcheck || $cnum != "13" || !$echeck || !$cindex || $cconf || !$pcheck) $actlink = "";
+  else $actlink = "choise";
+
+  if (eregi("linux",$OSTYPE)) {
+    if (file_exists("/etc/redhat-release")) $os_type = "Redhat";
+    elseif (file_exists("/etc/debian_version")) $os_type = "Debian";
+  } else $os_type = $OSTYPE;
+
+  echo "<form method=POST action=$PHP_SELF>\n\n" .
+       "<table width=400 border=0 cellpadding=5>\n" .
+       "<tr><td bgcolor=$color[l0_bg] align=center>\n" .
+       "<font color=$color[l0_fg]>JSBoard Enviornment Check Reuslt</font>\n" .
+       "</td></tr>\n<tr><td align=center>\n" .
+       "<font color=$color[text]>\n&nbsp;<br>\n\n" .
+       "<table>\n<tr>\n<td>OS Type</td>\n<td>:</td>\n<td>$os_type</td>\n</tr>\n\n";
+
+  if (!eregi("linux",$OSTYPE))
+    echo "<tr>\n<td colspan=3>\n<font color=red>$langs[os_check]</font>\n</td>\n</tr>\n\n";
+
+  echo "<tr>\n<td>MySQL check</td>\n<td>:</td>\n<td>$m</td>\n</tr>\n\n";
+
+  if (!$mcheck)
+    echo "<tr>\n<td colspan=3>\n<font color=red>$langs[mcheck]</font>\n</td>\n</tr>\n\n";
+  if ($mcheck)   
+    echo "<tr>\n<td>DB table column check</td>\n<td>:</td>\n<td>$mc</td>\n</tr>\n\n";
+  if ($mc == "Error")
+    echo "<tr>\n<td colspan=3>\n<font color=red>$langs[ccheck]</font>\n</td>\n</tr>\n\n";
+
+  echo "<tr>\n<td>exec() function check</td>\n<td>:</td>\n<td>$e</td>\n</tr>\n\n";
+
+  if ($e == "Failed")
+    echo "<tr>\n<td colspan=3>\n<font color=red>$langs[echeck]</font>\n</td>\n</tr>\n\n";
+
+  if ($echeck) {
+    echo "<tr>\n<td>index file check</td>\n<td>:</td>\n<td>$ci</td>\n</tr>\n\n";
+    if ($ci == "Failed") echo "<tr>\n<td colspan=3>\n<font color=red>$langs[icheck]</font>\n</td>\n</tr>\n\n";
+  }
+
+  echo "<tr>\n<td>Permission check</td>\n<td>:</td>\n<td>$p</td>\n</tr>\n\n";
+
+  if ($p == "Error") {
+    echo "<tr>\n<td colspan=3>\n<font color=red>$langs[pcheck]</font>\n</td>\n</tr>\n\n";
+  }
+
+  echo "</table>" .
+       "<br>&nbsp;\n</font>\n</td></tr>\n" .
+       "<tr><td bgcolor=$color[l0_bg] align=center>\n" .
+       "<input type=hidden name=mode value=$actlink>\n" .
+       "<input type=hidden name=langss value=$langs[code]>\n" .
+       "<input type=submit value=submit>\n" .
+       "</td></tr>\n</table>\n</form>\n";
+} elseif ($mode == "choise") {
+  echo "<form method=POST action=$PHP_SELF>\n\n" .
+       "<table width=400 border=0 cellpadding=5>\n" .
+       "<tr><td bgcolor=$color[l0_bg] align=center>\n" .
+       "<font color=$color[l0_fg]>JSBoard Installer</font>\n" .
+       "</td></tr>\n<tr><td align=center>\n" .
+       "<font color=$color[text]>\n&nbsp;<br>\n" .
+       "<input type=radio name=mode checked value=\"first\" id=radio>First Installation\n" .
+       "<input type=radio name=mode value=\"upgrade\" id=radio>Upgrade<br>\n" .
+       "<br>&nbsp;\n</font>\n</td></tr>\n" .
+       "<tr><td bgcolor=$color[l0_bg] align=center>\n" .
+       "<input type=hidden name=langss value=$langs[code]>\n" .
+       "<input type=submit value=submit>\n" .
+       "</td></tr>\n</table>\n</form>\n";
+} elseif ($mode == "first") {
+  if ($agent[br] == "MOZL") $fsize = form_size(7);
+  else $fsize = form_size(9);
+
+  echo "<form name=auth method=POST action=session.php>\n" .
+       "<table width=400 border=0 cellpadding=5>\n" .
+       "<tr><td bgcolor=$color[l0_bg] align=center>\n" .
+       "<font color=$color[l0_fg]>JSBoard Installer</font>\n" .
+       "</td></tr>\n<tr><td align=center>\n" .
+       "<font color=$color[text]>\nMySQL password<br>\n" .
+       "<input type=password name=mysqlpass size=$fsize>\n" .
+       "<input type=hidden name=mode value=login>\n" .
+       "<input type=hidden name=langss value=$langs[code]>\n" .
+       "</form>\n\n" .
+       "<form name=reset method=POST action=session.php>\n" .
+       "<input type=hidden name=mode value=logout>\n" .
+       "<input type=hidden name=langss value=$langs[code]>\n" .
+       "Password $langs[inst_r] <input type=submit name=reset value=reset>\n" .
+       "</font>\n</td></tr>\n" .
+       "<tr><td bgcolor=$color[l0_bg] align=center>\n" .
+       "<font color=$color[l0_fg]>$langs[auser]</font>\n" .
+       "</td></tr>\n</table>\n" .
+       "</form>\n";
+} elseif ($mode == "upgrade")
+  echo "<font size=+4><b> Comming Soon </b></font>\n\n";
+else
+  echo "<font size=+4><b> ^0^ </b></font>\n\n";
+
+echo "\n<p>\nCopyleft 1999-$copydate by " .
+     "<a href=http://jsboard.kldp.org target=_blank>JSBoard Open Project<a/>\n" .
+     "\n</td></tr>\n</table>\n";
+
+require("../html/tail.ph");
+?>
---
+++ admin/include/get.ph
@@ -0,0 +1,82 @@
+<?
+# 넷스케이프와 익스간의 FORM 입력창의 크기 차이를 보정하기 위한 함 수
+# intval - 변수를 정수형으로 변환함
+#          http://www.php.net/manual/function.intval.php
+function form_size($size, $print = 0) {
+  global $langs;
+
+  # 클라이언트 브라우져 종류를 가져오는 함수 (include/get_info.ph)
+  $agent = get_agent();
+
+  # 윈도우용 네스케이프
+  if($agent[br] == "MOZL") {
+    if($agent[os] == "NT") {
+      if($agent[ln] == "KO") $size *= 1.1; # 한글판
+      else {
+        if ($langs[code] == "ko") $size *= 2.6;
+        else $size *= 1.4;
+      }
+    } else if($agent[os] == "WIN") {
+      if($agent[ln] == "KO") $size *= 1.1; # 한글판
+      else $size *= 1.3;
+    } elseif($agent[os] == "LINUX") $size *= 1.0;
+  }
+
+  # 네스케이프 6
+  if($agent[br] == "MOZL6") {
+    if($agent[os] == "NT") {
+      if($agent[ln] == "KO") $size *= 1.1; # 한글판
+      else {
+        if ($langs[code] == "ko") $size *= 2.3;
+        else $size *= 1.4;
+      }
+    } else $size *= 1.3;
+  }
+
+  # 인터넷 익스플로러
+  if(eregi("MSIE",$agent[br])) {
+    if ($agent[os] == "NT")
+      if ($langs[code] == "ko") $size *= 2.3;
+      else $size *= 2.6;
+    else $size *= 2.3;
+  }
+
+  if($agent[br] == "LYNX") $size *= 2;
+  if($agent[br] == "KONQ") $size *= 2.6;
+
+  $size = intval($size);
+  if($print) echo $size;
+
+  return $size;
+}
+
+# table list 를 구한다.
+function get_tblist($db,$t="") {
+  $list = mysql_list_tables($db);
+
+  # table list 존재 유무 체크
+  table_list_check($db);
+
+  # table 의 총 갯수를 구함
+  $list_num = mysql_num_rows($list);
+  if(!$j) $j = 0;
+
+  for ($i=0;$i<$list_num;$i++) {
+    # table 이름을 구하여 배열에 저장
+    $l[$i] = mysql_tablename($list,$i);
+
+    # 배열에 저장된 이름중 알파벳별 구분 변수가 있으면 소트된
+    # 이름만 다시 배열에 저장
+    if($t) {
+      if(eregi("^$t",$l[$i])) {
+        $ll[$j] = $l[$i];
+        $j++;
+      }
+    }
+  }
+
+  if($t) return $ll;
+  else return $l;
+}
+
+?>
